fd32feb103d223beb24d7a732cbdd5992d240f4b
Hendrik <hendrik.wirthwein@gmail.com>
2021-04-09 09:42:08 +0200

    start project

---
+++ snakeGameSolution/snakeGame/src/Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+void test() {
+	cout << "this works" << endl;
+}
---
+++ snakeGameSolution/snakeGame/src/snakeGame.cpp
@@ -0,0 +1,13 @@
+// snakeGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hellooooooooooooooooooooooooo World!\n";
+}
+
